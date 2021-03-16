339fb7c5dba9d1a7be0bab015f860a99729e3c30
Fynn Faber <faberf@pop-os.localdomain>
2021-03-16 23:03:09 +0100

    hot

--- Experiments/saturation_experiment.py
+++ Experiments/saturation_experiment.py
@@ -1,8 +1,14 @@
 from architectures import ecg_lsnn
-from datajuicer import configure
+from datajuicer import configure, run
 
 class saturation_experiment:
-    def train():
+    @staticmethod
+    def train_grid():
         grid = [ecg_lsnn.make()]
         grid = configure(grid, {"eval_step_interval":25})
         return grid
+    
+    @staticmethod
+    def visualize():
+        grid = saturation_experiment.train_grid()
+        grid = run(grid, "train", run_mode="load", store_key="*")("{*}")
