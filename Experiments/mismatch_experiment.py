982c4e8860c908bf2124d3f62e7a318f52cb44c7
Fynn Faber <faberf@pop-os.localdomain>
2021-05-15 22:56:01 +0200

    for leonhard

--- Experiments/mismatch_experiment.py
+++ Experiments/mismatch_experiment.py
@@ -21,7 +21,8 @@ class mismatch_experiment:
         ecg4 = configure(ecg, {"beta_robustness": 0.0, "noisy_forward_std":0.3})
         #ecg5 = configure(ecg, {"beta_robustness": 0.0, "optimizer":"abcd", "abcd_L":2, "n_epochs":"40,10", "learning_rate":"0.001,0.0001"})
         ecg6 = configure(ecg, {"beta_robustness": 0.0, "awp":True, "boundary_loss":"madry", "awp_gamma":0.1})
-        ecg = ecg0 + ecg1 + ecg2 + ecg3 + ecg4  + ecg6
+        ecg7 = configure(ecg, {"beta_robustness": 0.25, "attack_size_mismatch": 0.1, "noisy_forward_std":0.3})
+        ecg = ecg0 + ecg1 + ecg2 + ecg3 + ecg4  + ecg6 + ecg7
 
         speech = [speech_lsnn.make()]
         speech0 = configure(speech, {"beta_robustness": 0.0})
@@ -31,7 +32,8 @@ class mismatch_experiment:
         speech4 = configure(speech, {"beta_robustness": 0.0, "noisy_forward_std":0.3})
         #speech5 = configure(speech, {"beta_robustness": 0.0, "optimizer":"abcd", "abcd_L":2, "n_epochs":"40,10", "learning_rate":"0.001,0.0001"})
         speech6 = configure(speech, {"beta_robustness": 0.0, "awp":True, "boundary_loss":"madry", "awp_gamma":0.1})
-        speech = speech0 + speech1 + speech2 + speech3 + speech4  + speech6
+        speech7 = configure(speech, {"beta_robustness": 0.25, "attack_size_mismatch": 0.1, "noisy_forward_std":0.3})
+        speech = speech0 + speech1 + speech2 + speech3 + speech4  + speech6 + speech7
 
         cnn_grid = [cnn.make()]
         cnn_grid0 = configure(cnn_grid, {"beta_robustness": 0.0})
@@ -41,9 +43,10 @@ class mismatch_experiment:
         cnn_grid4 = configure(cnn_grid, {"beta_robustness": 0.0, "noisy_forward_std":0.3})
         #cnn_grid5 = configure(cnn_grid, {"beta_robustness": 0.0, "optimizer":"abcd", "abcd_L":2, "n_epochs":"10,2"})
         cnn_grid6 = configure(cnn_grid, {"beta_robustness":0.0, "awp":True, "awp_gamma":0.1, "boundary_loss":"madry"})
-        cnn_grid = cnn_grid0 + cnn_grid1 + cnn_grid2 + cnn_grid3 + cnn_grid4  + cnn_grid6
+        cnn_grid7 = configure(cnn_grid, "beta_robustness": 0.25, "attack_size_mismatch": 0.1, "noisy_forward_std":0.3})
+        cnn_grid = cnn_grid0 + cnn_grid1 + cnn_grid2 + cnn_grid3 + cnn_grid4  + cnn_grid6 + cnn_grid7
 
-        final_grid = ecg + speech + cnn_grid
+        final_grid = ecg + speech #+ cnn_grid
         final_grid = split(final_grid, "seed", seeds)
 
         return final_grid
