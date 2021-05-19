7c6de3c4d4f96cd0d64e74905d52ebe65e452972
Fynn Faber <faberf@pop-os.localdomain>
2021-05-19 11:11:03 +0200

    hot

--- merge.py
+++ merge.py
@@ -6,36 +6,43 @@ import os
 print("merging...")
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-sessions_dirs", nargs="+", default=[])
-ap.add_argument("-resources_dirs", nargs="+", default=[])
+ap.add_argument("-sessions_dirs", nargs="*", default=[])
+ap.add_argument("-resources_dirs", nargs="*", default=[])
 
 flags =ap.parse_args()
 
 sessions_dirs = [sd[0:-1] if sd[-1]=="/" else sd for sd in flags.sessions_dirs]
 resources_dirs = [rd[0:-1] if rd[-1]=="/" else rd for rd in flags.resources_dirs]
 
-os.system("cp " + sessions_dirs[0]+ "/sessions.db "+ "combined_tmp.db")
+merge_sessions = len(sessions_dirs)>1
+merge_resources = len(resources_dirs)>1
 
-con3 = sqlite3.connect("combined_tmp.db")
+if merge_sessions:
 
-for sd in sessions_dirs[1:]:
-    print("dba = " + str(sd+"/sessions.db"))
-    if not os.path.isfile(sd+"/sessions.db"):
-        continue
-    con3.execute("ATTACH '"+ str(sd+"/sessions.db") + "' as dba")
+    os.system("cp " + sessions_dirs[0]+ "/sessions.db "+ "combined_tmp.db")
 
-    con3.execute("BEGIN")
-    for row in con3.execute("SELECT * FROM dba.sqlite_master WHERE type='table'"):
-        combine = "INSERT OR IGNORE INTO "+ row[1] + " SELECT * FROM dba." + row[1]
-        print(combine)
-        con3.execute(combine)
-    con3.commit()
-    con3.execute("detach database dba")
+    con3 = sqlite3.connect("combined_tmp.db")
 
-con3.close()
+    for sd in sessions_dirs[1:]:
+        print("dba = " + str(sd+"/sessions.db"))
+        if not os.path.isfile(sd+"/sessions.db"):
+            continue
+        con3.execute("ATTACH '"+ str(sd+"/sessions.db") + "' as dba")
 
-os.system("rsync -abviuzP " + " ".join([sd+'/' for sd in sessions_dirs]))
-os.system("rsync -abviuzP " + " ".join([rd+'/' for rd in resources_dirs]))
+        con3.execute("BEGIN")
+        for row in con3.execute("SELECT * FROM dba.sqlite_master WHERE type='table'"):
+            combine = "INSERT OR IGNORE INTO "+ row[1] + " SELECT * FROM dba." + row[1]
+            print(combine)
+            con3.execute(combine)
+        con3.commit()
+        con3.execute("detach database dba")
+
+    con3.close()
+
+    os.system("rsync -abviuzP " + " ".join([sd+'/' for sd in sessions_dirs]))
+    os.system(f"mv combined_tmp.db {sessions_dirs[-1]}/sessions.db")
+
+if merge_resources:
+    os.system("rsync -abviuzP " + " ".join([rd+'/' for rd in resources_dirs]))
 
 
-os.system(f"mv combined_tmp.db {sessions_dirs[-1]}/sessions.db")
