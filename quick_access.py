c4f1217e55db19d4702d3137dc9065326b3aa996
Julian Buechel <jbu@zurich.ibm.com>
2021-11-10 19:09:01 +0100

    Adapted DB struc

--- quick_access.py
+++ quick_access.py
@@ -7,14 +7,16 @@ from Experiments import (
     methods_random_experiment,
     IBP_experiment,
     landscape_vary_beta_experiment,
-    methods_random_vary_beta_experiment)
+    methods_random_vary_beta_experiment,
+    awp_experiment)
 
 # mismatch_experiment.mismatch_experiment.visualize()
 # worst_case_experiment.worst_case_experiment.visualize()
 # methods_experiment.methods_experiment.visualize()
-weight_scale_experiment.weight_scale_experiment.visualize()
+# weight_scale_experiment.weight_scale_experiment.visualize()
 # landscape_experiment.landscape_experiment.visualize()
 # methods_random_experiment.methods_random_experiment.visualize()
 # IBP_experiment.IBP_experiment.visualize()
 # landscape_vary_beta_experiment.landscape_vary_beta_experiment.visualize()
 # methods_random_vary_beta_experiment.methods_random_vary_beta_experiment.visualize()
+awp_experiment.awp_experiment.visualize()
