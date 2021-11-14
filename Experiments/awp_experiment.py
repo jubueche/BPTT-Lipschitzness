ff954839de45e2307ff23362ff76f4d76abfb0ff
Julian Buechel <jbu@zurich.ibm.com>
2021-11-14 09:06:27 +0100

    Run ecg

--- Experiments/awp_experiment.py
+++ Experiments/awp_experiment.py
@@ -30,19 +30,19 @@ class awp_experiment:
         speech_awp_eps = split(speech_awp, "eps_pga", eps_pgas)
         speech_awp_eps = configure(speech_awp_eps, {"nb_iter":3})
 
-        final_grid = cnn_awp + cnn_awp_eps
+        final_grid = ecg_awp + ecg_awp_eps
         final_grid = split(final_grid, "seed", seeds)
         return final_grid
 
     @staticmethod
     def visualize():
 
-        speech_mm_levels = [0.0,0.1,0.2,0.3,0.5,0.7]
+        mm_levels = [0.0,0.1,0.2,0.3,0.5,0.7]
 
         grid = [model for model in awp_experiment.train_grid() if model["seed"] in seeds] 
         grid = run(grid, "train", run_mode="load", store_key="*")("{*}")
 
-        grid_mm = split(grid, "mm_level", speech_mm_levels, where={"architecture":"speech_lsnn"})
+        grid_mm = split(grid, "mm_level", mm_levels)
 
         grid_mm = configure(grid_mm, {"mode":"direct", "n_iterations":100})    
         grid_mm = run(grid_mm, get_mismatch_list, n_threads=10, store_key="mismatch_list")\
@@ -55,13 +55,13 @@ class awp_experiment:
 
         eps_pgas_list = [0] + eps_pgas
         for eps_pga in eps_pgas_list:
-            accs = np.empty(shape=(len(speech_mm_levels),))
-            stds = np.empty(shape=(len(speech_mm_levels),))
-            for idx,mm_level in enumerate(speech_mm_levels):
+            accs = np.empty(shape=(len(mm_levels),))
+            stds = np.empty(shape=(len(mm_levels),))
+            for idx,mm_level in enumerate(mm_levels):
                 accuracy_list = query(grid_mm, "mismatch_list", where={"eps_pga":eps_pga, "mm_level":mm_level})
                 accs[idx] = np.mean(accuracy_list)
                 stds[idx] = np.std(accuracy_list)
-            ax.errorbar(x=speech_mm_levels, y=accs, yerr=stds, label=r"$\epsilon_{pga}=$" + ("%.2f"%eps_pga))
+            ax.errorbar(x=mm_levels, y=accs, yerr=stds, label=r"$\epsilon_{pga}=$" + ("%.2f"%eps_pga))
         
         plt.legend()
         plt.show()
